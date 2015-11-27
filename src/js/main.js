e85246194cea0b18c0f30bfcde2005f8dad98d7a
Kaan Karabey <karabeykaan@gmail.com>
2015-11-27 04:16:51 +0200

    products are rising,simple product object working and started to add products to our inventory- yavaş yavaş

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+
+
+
 console.log("Thats OK");
 
 var	categories = ["liveKits", "petProducts","forHumans"];
@@ -18,41 +21,40 @@ var productList = [];
 
 
 
-
-var Product = function(pName,pId) {
-	this.pname=pName;
-	this.pid = pId;
+//Constructor object
+var Product = function(Name,cathegory,desc) {
+	this.Name=Name;
+	this.cathegory = cathegory;
+	this.desc = desc;
 
 };
-
-var createProduct = new Product("kaan","13");
-
-//function addProductList() {
+//WILL be created automated with form elements.Just examples
+var createProduct = new Product("kaan",categories[0],"nice Product");
+var createProduct2 = new Product("kaan1",categories[1],"nice one too");
 	productList.push(createProduct);
-console.log("productList12345" + productList);	
-//};
+	productList.push(createProduct2);
+	
+	console.log("created Product"+ createProduct.Name + " " +createProduct.cathegory);
+	console.log(productList);
 
 
 
 
+	//For view of Products that we have 
+var existingProducts = document.getElementById('existingProductsList');
+for(var i=0;i<productList.length;i++){
+var listItem = document.createElement('li');
+	listItem.appendChild(document.createTextNode(productList[i].Name));
+	existingProducts.appendChild(listItem);
+}
 
+//add new product to your inventory
+var addProduct = function(){
+	var newProduct = new Product("ahmet",categories[2],"not bad");
+	var productLi = document.createElement('li');
+	
+	productLi.appendChild(document.createTextNode(newProduct.Name));
+	existingProducts.appendChild(productLi);
 
-
-
-
-var fPlayers = [];
-
-var fPlayer = {
-	id : 1,
-	name: "Kaan",
-	surname: "Karabey",
-	age: 13,
-	position: "dmc",
-};
-
-console.log(fPlayer.name);
-
-fPlayers.push(fPlayer);
-
-console.log(fPlayers);
-
+};	
+	
